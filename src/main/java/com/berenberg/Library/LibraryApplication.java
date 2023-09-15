ba934749eea813aed451bbfede4785600940366f
Shishir Lakkadi <shishir.lakkadi@gmail.com>
2023-09-15 16:59:37 +0100

    Removed unwanted files

--- src/main/java/com/berenberg/Library/LibraryApplication.java
+++ src/main/java/com/berenberg/Library/LibraryApplication.java
@@ -2,10 +2,11 @@ package com.berenberg.Library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LibraryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
